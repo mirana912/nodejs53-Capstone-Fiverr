32a38c5008d2b478b3ee18bd047d7686bad02a01
Mirana912 <mirana912@gmail.com>
2026-03-04 21:11:33 +0700

    abc

--- backend/prisma.config.js
+++ backend/prisma.config.js
@@ -1,8 +1,7 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 export default {
   schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-  },
   datasource: {
     url: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   },
